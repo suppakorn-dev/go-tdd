1f74e803f2590ce6318964d6d6855475b048877c
Tanakorn Wiriya-Aree <tanakorn.wir@ascendcorp.com>
2022-09-13 18:57:09 +0700

    feat: Operator 2, 3 return following values -, /

--- captcha/operator.go
+++ captcha/operator.go
@@ -3,5 +3,13 @@ package captcha
 type Operator int
 
 func (o Operator) String() string {
+	if o == 3 {
+		return "/"
+	}
+
+	if o == 2 {
+		return "-"
+	}
+
 	return "+"
 }
--- captcha/operator_test.go
+++ captcha/operator_test.go
@@ -9,3 +9,19 @@ func Test_whenOperatorIs1_stringShouldReturnPlus(t *testing.T) {
 		t.Errorf("Operator.String() should return + but got %s", o.String())
 	}
 }
+
+func Test_whenOperatorIs2_stringShouldReturnMinus(t *testing.T) {
+	var o Operator = 2
+
+	if o.String() != "-" {
+		t.Errorf("Operator.String() should return + but got %s", o.String())
+	}
+}
+
+func Test_whenOperatorIs3_stringShouldReturnDivide(t *testing.T) {
+	var o Operator = 3
+
+	if o.String() != "/" {
+		t.Errorf("Operator.String() should return + but got %s", o.String())
+	}
+}
