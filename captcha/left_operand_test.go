efaa188b2f40e5d48a60407e5653f98fbcbe174a
Tanakorn Wiriya-Aree <tanakorn.wir@ascendcorp.com>
2022-10-04 19:10:22 +0700

    feat: [go-tdd][Fram,Bas] add leftOperand factory when pattern is 2

--- captcha/left_operand_test.go
+++ captcha/left_operand_test.go
@@ -13,3 +13,15 @@ func TestNewLeftOperandWhenPatternIs1ShouldReturnIntOperand(t *testing.T) {
 		t.Errorf("Expect type IntOperand but got %T", sut)
 	}
 }
+
+func Test_NewLeftOperand_whenPatternIs2_shouldReturnWordOperand(t *testing.T) {
+	pattern := 2
+	leftOperand := 1
+
+	sut := NewLeftOperand(pattern, leftOperand)
+
+	_, ok := sut.(WordOperand)
+	if !ok {
+		t.Errorf("Expect type WordOperand but got %T", sut)
+	}
+}
--- captcha/left_operrand.go
+++ captcha/left_operrand.go
@@ -1,5 +1,8 @@
 package captcha
 
 func NewLeftOperand(pattern, rawValue int) Stringable {
+	if pattern == 2 {
+		return WordOperand(rawValue)
+	}
 	return IntOperand(rawValue)
 }
